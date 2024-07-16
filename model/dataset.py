48cffa121fcfbf98cdd6f3f2b65f9fa0d72a970d
yaober <jia.yao@utsouthwestern.edu>
2024-07-16 11:02:29 -0500

    fix ig score

--- model/dataset.py
+++ model/dataset.py
@@ -1,9 +1,8 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import numpy as np
 import torch
-import pandas as pd
-from scipy.spatial.distance import cdist
 import scipy.sparse as sp
+from scipy.spatial.distance import cdist
 from tqdm import trange
 
 class BagsDataset(Dataset):
@@ -12,10 +11,10 @@ class BagsDataset(Dataset):
         self.max_instances = max_instances
         self.resolution = resolution
         if isinstance(adata_radius_input, list):
-            self.bags = self.create_bags(adata_radius_input)
+            self.bags, self.gene_names = self.create_bags(adata_radius_input)
         else:
             assert radius is not None, "When a single adata is provided, radius must also be provided."
-            self.bags = self.create_bags([(adata_radius_input, radius)])
+            self.bags, self.gene_names = self.create_bags([(adata_radius_input, radius)])
 
     def __len__(self):
         return len(self.bags)
@@ -29,12 +28,13 @@ class BagsDataset(Dataset):
         else:
             gene_expression = torch.tensor(gene_expression, dtype=torch.float32)
         label = torch.tensor(bag['label'], dtype=torch.float32)
-        core_idx = bag['core_idx']  # Add core_idx to the output
-        return distances, gene_expression, label, core_idx
+        core_idx = bag['core_idx']
+        return distances, gene_expression, label, core_idx, self.gene_names
 
     def create_bags(self, adata_radius_list):
         bags = {}
-        bag_id = 0  # Initialize bag_id to ensure continuous IDs starting from 0
+        bag_id = 0
+        gene_names = None
 
         for adata, radius in adata_radius_list:
             spatial_coords_x = adata.obs['X'].astype(float)
@@ -50,25 +50,27 @@ class BagsDataset(Dataset):
             cell_types = adata.obs['cell_type'].values
             barcodes = adata.obs.index.values
 
+            # Store gene names
+            if gene_names is None:
+                gene_names = adata.var_names.tolist()
+
             for i in trange(len(spatial_coords), desc=f"Creating Bags with radius {radius}", ncols=100, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]"):
                 if cell_types[i] == 0:
-                    continue  # Skip if the cell type is 0
+                    continue
 
                 dist_matrix_row = cdist([spatial_coords[i]], spatial_coords, metric='euclidean')[0]
                 in_circle = np.where(dist_matrix_row <= radius)[0]
-                in_circle = [idx for idx in in_circle if cell_types[idx] != 0]  # Filter based on cell type
+                in_circle = [idx for idx in in_circle if cell_types[idx] != 0]
 
                 if self.resolution == 'high':
-                    # Only include the core instance if its cell type is 1
                     if cell_types[i] == 1:
                         in_circle.append(i)
                     else:
                         in_circle = [idx for idx in in_circle if idx != i]
 
                 if len(in_circle) == 0:
-                    continue  # Skip if no instances meet the criteria
+                    continue
 
-                # Drop the bag if the number of instances exceeds max_instances
                 if self.max_instances is not None and len(in_circle) > self.max_instances:
                     continue
 
@@ -79,21 +81,20 @@ class BagsDataset(Dataset):
                     'distances': distances,
                     'gene_expression': gene_data,
                     'label': labels[i],
-                    'core_idx': i  # Store the core index
+                    'core_idx': i
                 }
 
-                bag_id += 1  # Increment bag_id for the next bag
+                bag_id += 1
 
         total_bags = len(bags)
         avg_instances_per_bag = sum(bags[i]['gene_expression'].shape[0] for i in bags) / total_bags if total_bags > 0 else 0
         print(f"Total bags created: {total_bags}")
         print(f"Average instances per bag: {avg_instances_per_bag:.0f}")
 
-        return bags
-
+        return bags, gene_names
 
 def custom_collate_fn(batch):
-    distances, gene_expressions, labels, core_idxs = zip(*batch)
+    distances, gene_expressions, labels, core_idxs, gene_names = zip(*batch)
     distances = [torch.tensor(np.array(d), dtype=torch.float32) for d in distances]
     gene_expressions_tensors = []
     for g in gene_expressions:
@@ -102,5 +103,7 @@ def custom_collate_fn(batch):
         else:
             gene_expressions_tensors.append(g.clone().detach().float())
     labels = torch.tensor(labels, dtype=torch.float32)
-    core_idxs = torch.tensor(core_idxs, dtype=torch.long)  # Ensure core_idxs are tensors of type long
-    return distances, gene_expressions_tensors, labels, core_idxs
+    core_idxs = torch.tensor(core_idxs, dtype=torch.long)
+    # All gene_names should be the same, so we can just take the first one
+    current_genes = gene_names[0]
+    return distances, gene_expressions_tensors, labels, core_idxs, current_genes
