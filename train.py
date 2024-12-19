87171d59ef4b5cb3d94ad47b0814983e88ba2f51
yaober <jia.yao@utsouthwestern.edu>
2024-12-19 13:28:53 -0600

    adamw k=2

--- train.py
+++ train.py
@@ -71,7 +71,7 @@ def train_model(args):
         immune_cell=args.immune_cell,
         max_instances=args.max_instances,
         n_genes=args.n_genes,
-        k=4  # Ensure 'k' matches the number of bags per batch
+        k=2  # Ensure 'k' matches the number of bags per batch
     )
     train_size = int(0.7 * len(dataset))
     val_size = len(dataset) - train_size
