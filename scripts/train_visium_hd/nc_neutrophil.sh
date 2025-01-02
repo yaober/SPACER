7e2615cc9437997e7077c9d99ed7f1f674e793ce
yaober <jia.yao@utsouthwestern.edu>
2025-01-02 13:47:38 -0600

    cancel relative rank

--- scripts/train_visium_hd/nc_neutrophil.sh
+++ scripts/train_visium_hd/nc_neutrophil.sh
@@ -17,4 +17,4 @@
 source activate spatial_tcr
 
 cd /project/DPDS/Wang_lab/s439765/spatial_tcr/MIL_TCR
-python train.py --data data/visiumhd/neutrophil.csv --reference_gene data/human_filtered.csv --output_dir finalize_model_visiumhd_top500_relative/neutrophil --immune_cell neutrophil --learning_rate 0.05 --num_epochs 10 --patience 5 --delta 0.0001  --n_genes 500    
+python train.py --data data/visiumhd/neutrophil.csv --reference_gene data/human_filtered.csv --output_dir finalize_model_visiumhd_top500/neutrophil --immune_cell neutrophil --learning_rate 0.05 --num_epochs 10 --patience 5 --delta 0.0001  --n_genes 500    
